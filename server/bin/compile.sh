f437bc6c5efcc18de8a4260d9a045d74a2d160f3
Sam Zagrobelny <Samuel.Zagrobelny@dayautomation.com>
2016-09-12 13:10:58 -0400

    added manifest

--- server/bin/compile.sh
+++ server/bin/compile.sh
@@ -5,4 +5,5 @@ jade -o '../../docs/' -O '{"static":true}' -P ../views/index.jade
 mkdir -p ../../docs/{javascripts,stylesheets}/
 lessc ../public/stylesheets/style.less > ../../docs/stylesheets/style.css
 cat ../public/javascripts/{models,bluetooth,adapters,index}.js > ../../docs/javascripts/index.js
+cp ../public/manifest.json ../../docs/manifest.json
 #cp ../public/javascripts/* ../../docs/javascripts/
