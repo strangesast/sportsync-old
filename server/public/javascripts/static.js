415c3877f6e213b29e42cd0c7e5ba87d2fdd175c
Sam Zagrobelny <Samuel.Zagrobelny@dayautomation.com>
2016-09-15 15:45:39 -0400

    progress

---
+++ server/public/javascripts/static.js
@@ -0,0 +1,14 @@
+window.addEventListener('scroll', function(e) {
+  var s = window.scrollY;
+  if(s < 1) {
+    document.querySelector('.pages').classList.remove('short');
+    document.querySelector('.wide').classList.remove('right');
+  } else {
+    if(!document.querySelector('.pages').classList.contains('short')) {
+      window.scrollTo(0, 1);
+      document.querySelector('.pages').classList.add('short');
+      document.querySelector('.wide').classList.add('right');
+      e.preventDefault();
+    }
+  }
+});
