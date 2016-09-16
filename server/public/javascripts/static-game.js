43258d91b9718d126e13dbf20e8b0140f1b71a26
Sam Zagrobelny <Samuel.Zagrobelny@dayautomation.com>
2016-09-16 13:31:18 -0400

    crude listener

--- server/public/javascripts/static-game.js
+++ server/public/javascripts/static-game.js
@@ -13,7 +13,18 @@ window.addEventListener('scroll', function(e) {
   }
 });
 
-document.querySelector('.fa-bars').addEventListener('click', function(e) {
-  document.querySelector('.dropdown').classList.toggle('active');
-  document.querySelector('body').classList.toggle('full');
+document.querySelector('.fa-bars').addEventListener('click', function() {
+  if(!document.querySelector('.dropdown').classList.contains('active')) {
+    document.querySelector('.dropdown').classList.add('active');
+    document.querySelector('body').classList.add('full');
+
+    var func = function(e) {
+      document.querySelector('.dropdown').classList.remove('active');
+      document.querySelector('body').classList.remove('full');
+      window.removeEventListener('click', func, false);
+    };
+    setTimeout(function() {
+      window.addEventListener('click', func, false);
+    }, 10);
+  }
 });
